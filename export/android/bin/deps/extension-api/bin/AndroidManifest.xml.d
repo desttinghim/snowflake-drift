98ae994c352d21c7a32be3d909bdcd9c88597539
Louis Pearson <desttinghimgame@gmail.com>
2016-06-01 14:50:52 -0500

    Removed export files from git. :/

--- export/android/bin/deps/extension-api/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\epearson\Documents\GitHub\snowflake-drift\export\android\bin\deps\extension-api\bin\AndroidManifest.xml : \
-C:\Users\epearson\Documents\GitHub\snowflake-drift\export\android\bin\deps\extension-api\AndroidManifest.xml \
--- export/android/bin/deps/extension-api/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : C:\Users\epearson\Documents\GitHub\snowflake-drift\export\android\bin\deps\extension-api\bin\AndroidManifest.xml \
